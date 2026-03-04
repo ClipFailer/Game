4459b2c78be5055437183626451b8d76580507ba
clipfail <rogroty@gmail.com>
2026-03-04 17:30:38 +0400

    Исправление ошибок

--- Source/Platformer/Public/Characters/MainCharacter/MainCharacter.h
+++ Source/Platformer/Public/Characters/MainCharacter/MainCharacter.h
@@ -37,65 +37,65 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	
+
 	//===============================================================
 	//			                METHODS
 	//===============================================================
-	
+
 	//---------------------------------------------------------------
 	//					Movement & Interaction
 	//---------------------------------------------------------------
-	
+
 	/// @brief Выполняется в начале игры
 	virtual void BeginPlay() override;
-	
+
 	/**
 	 * @brief Обрабатывает действие по его типу.
-	 * 
+	 *
 	 * Отвечает соответствующим методом на входящий тип события и тип тригера
-	 * 
+	 *
 	 * @param TriggerEvent TriggerEvent, запустивший событие Action
 	 * @param ActionType Тип события для обработки
-	 * 
+	 *
 	 * @see EInteractionType
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void HandleAction(ETriggerEvent TriggerEvent, EInteractionType ActionType);
-	
+
 	/**
 	 * Выполняет взаимодействие с интерактивным эктором
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void OnInteractActionStarted();
-	
+
 	/**
 	 * Обрабатывает окончание второстепенного действия
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void OnPrimaryActionStarted();
-	
+
 	/**
 	 * Обрабатывает окончание второстепенного действия
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void OnPrimaryActionCompleted();
-	
+
 	/**
 	 * @brief Обрабатывает начало второстепенного действия
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void OnSecondaryActionStarted();
-	
+
 	/**
 	 * @brief Обрабатывает окончание второстепенного действия
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void OnSecondaryActionCompleted();
-	
+
 	//===============================================================
 	//			                PROPERTIES
 	//===============================================================
-	
+
 	// Компонент камеры
 	UPROPERTY(EditDefaultsOnly, Category = "Camera")
 	UCameraComponent* CameraComponent;
@@ -103,7 +103,7 @@ protected:
 	// Компонент обработки ввода персонажа
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Input")
 	UCharacterInputComponent* CharacterInputComponent;
-	
+
 	// Компонент, отвечающий за взаимодействие персонажа с миром
 	UPROPERTY(EditDefaultsOnly, Category = "Interaction")
 	UInteractionComponent* InteractionComponent;
